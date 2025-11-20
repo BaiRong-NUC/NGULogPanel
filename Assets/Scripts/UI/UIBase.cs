b08158d9e991c3bf82c7fcd4722ea866b7860553
BaiRong-NUC <gby@edu.nuc.email>
2025-11-20 21:51:44 +0800

    创建了UI面板基类

---
+++ Assets/Scripts/UI/UIBase.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class UIBase<T> : MonoBehaviour where T : class
+{
+    private static T _instance;
+
+    public static T instance => _instance;
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public virtual void Show()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public virtual void Hide()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    protected virtual void Start()
+    {
+        this.Init();
+    }
+
+    //子类继承时必须实现初始化方法
+    public abstract void Init();
+}
