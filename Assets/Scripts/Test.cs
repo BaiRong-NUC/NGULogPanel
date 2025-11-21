d02daa0ad5e4b8d2d3130a0ffc580e0f9d0a8a5c
BaiRong-NUC <gby@edu.nuc.email>
2025-11-21 15:48:19 +0800

    完成提示面板的封装

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            TipPanel.instance.Show();
+            TipPanel.instance.ChangeContent("测试提示内容!");
+        }
+    }
+}
--- Assets/Scripts/UI/TipPanel.cs
+++ Assets/Scripts/UI/TipPanel.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class TipPanel : UIBase<TipPanel>
 {
+    public UIButton BtnSure; // 确认按钮
+    public UILabel LabContent; // 提示内容
+
     public override void Init()
     {
-        
-        
+        BtnSure.onClick.Add(new EventDelegate(() =>
+        {
+            this.Hide();
+        }));
+
+        this.Hide(); // 初始化时隐藏面板
+    }
+
+    // 更改提示内容
+    public void ChangeContent(string content)
+    {
+        LabContent.text = content;
     }
 }
