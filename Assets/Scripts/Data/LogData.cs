d0a253be1834a3a10dee6124090bad3d51fc8c6c
BaiRong-NUC <gby@edu.nuc.email>
2025-11-21 17:46:43 +0800

    完成登录界面信息存储与读取逻辑

---
+++ Assets/Scripts/Data/LogData.cs
@@ -0,0 +1,28 @@
+﻿// 玩家登录数据
+public class LogData
+{
+    public string userName;
+    public string password;
+    public bool isRemember;
+    public bool isAutoLogin;
+    public int serverId;
+}
+
+public class LogDataManager
+{
+    private static LogDataManager _instance = new LogDataManager();
+    public static LogDataManager instance => _instance;
+    private LogDataManager()
+    {
+        // this.logData = (LogData)XmlDataManage.instance.LoadData(typeof(LogData), "LogData");
+        this.logData = XmlDataManage.instance.LoadData(typeof(LogData), "LogData") as LogData;
+
+    }
+
+    public LogData logData;
+
+    public void SaveLogData()
+    {
+        XmlDataManage.instance.SaveData(this.logData, "LogData");
+    }
+}
---
+++ Assets/Scripts/UI/LogPanel.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogPanel : UIBase<LogPanel>
+{
+    public UIInput userNameInput;
+    public UIInput passwordInput;
+    public UIButton btnLogin;
+    public UIButton btnRegister;
+    public UIToggle rememberPasswordToggle;
+    public UIToggle autoLoginToggle;
+    public override void Init()
+    {
+        // 注册事件
+        this.btnRegister.onClick.Add(new EventDelegate(() =>
+        {
+            Debug.Log("注册按钮被点击");
+            this.Hide();
+        }));
+        this.btnLogin.onClick.Add(new EventDelegate(() =>
+        {
+            // Debug.Log("登录按钮被点击");
+            // 判断用户名和密码是否为正确
+
+            // 记录数据
+            LogDataManager.instance.logData.userName = this.userNameInput.value;
+            LogDataManager.instance.logData.password = this.passwordInput.value;
+            LogDataManager.instance.logData.isRemember = this.rememberPasswordToggle.value;
+            LogDataManager.instance.logData.isAutoLogin = this.autoLoginToggle.value;
+            LogDataManager.instance.SaveLogData();
+        }));
+        this.rememberPasswordToggle.onChange.Add(new EventDelegate(() =>
+        {
+            if (this.rememberPasswordToggle.value == false)
+            {
+                this.autoLoginToggle.value = false;
+            }
+        }));
+        this.autoLoginToggle.onChange.Add(new EventDelegate(() =>
+        {
+            if(this.autoLoginToggle.value == true)
+            {
+                this.rememberPasswordToggle.value = true;
+            }
+        }));
+
+        // 初始化登录界面数据
+        LogData logData = LogDataManager.instance.logData;
+        this.rememberPasswordToggle.value = logData.isRemember;
+        this.autoLoginToggle.value = logData.isAutoLogin;
+        this.userNameInput.value = logData.userName;
+        if (this.rememberPasswordToggle.value == true)
+        {
+            this.passwordInput.value = logData.password;
+        }
+        if (this.autoLoginToggle.value == true)
+        {
+            Debug.Log("自动登录中...");
+        }
+    }
+}
