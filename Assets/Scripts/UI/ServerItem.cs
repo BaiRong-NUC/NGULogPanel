f82042f5214db0613f7184d9c217bc9f5856a823
BaiRong-NUC <gby@edu.nuc.email>
2025-11-24 19:47:09 +0800

    完成服务器按钮基本框架

---
+++ Assets/Scripts/UI/ServerItem.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerItem : MonoBehaviour
+{
+    public UIButton button; // 选择服务器按钮
+    public UILabel label; // 服务器名称
+    public UISprite newServerSprite; // 新服标识
+    public UISprite serverStatusSprite; // 服务器状态图标
+
+    public ServerInfo serverInfo; // 服务器信息
+
+    void Start()
+    {
+        this.button.onClick.Add(new EventDelegate(() =>
+        {
+            // 选择这个服务器
+            print("ServerItem button clicked");
+
+            // 隐藏选服面板
+
+            // 显示服务器面板
+
+            // 更新服务器面板
+        }));
+    }
+
+    public void SetLabelString(ServerInfo serverInfo)
+    {
+        this.serverInfo = serverInfo;
+        //31区    天下无双
+        this.label.text = serverInfo.id + "区    " + serverInfo.name;
+        this.newServerSprite.gameObject.SetActive(serverInfo.isNew);
+
+        // 服务器状态 0-无状态 1-流畅 2-繁忙 3-火爆 4-维护 
+        switch (serverInfo.status)
+        {
+            case 0:
+                this.serverStatusSprite.gameObject.SetActive(false);
+                break;
+            case 1:
+                this.serverStatusSprite.spriteName = "ui_DL_liuchang_01";
+                break;
+            case 2:
+                this.serverStatusSprite.spriteName = "ui_DL_fanhua_01";
+                break;
+            case 3:
+                this.serverStatusSprite.spriteName = "ui_DL_huobao_01";
+                break;
+            case 4:
+                this.serverStatusSprite.spriteName = "ui_DL_weihu_01";
+                break;
+        }
+    }
+}
