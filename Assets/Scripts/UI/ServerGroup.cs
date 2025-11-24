a45f92581ef4e486926ae3531761d97df116ce10
BaiRong-NUC <gby@edu.nuc.email>
2025-11-24 19:22:43 +0800

    完成选服务器范围按钮基本框架

---
+++ Assets/Scripts/UI/ServerGroup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 选择服务器范围按钮
+public class ServerGroup : MonoBehaviour
+{
+    public UIButton button; // 选择服务器范围按钮
+    public UILabel label; // 服务器范围标签
+
+    // 选择的服务器ID范围
+    public int begin;
+    public int end;
+
+    void Start()
+    {
+        this.button.onClick.Add(new EventDelegate(() =>
+        {
+            print("ServerGroup button clicked");
+            // 更新服务器展示的内容
+        }));
+    }
+
+    public void SetLabelString(int begin, int end)
+    {
+        this.label.text = string.Format("{0} - {1}区", begin, end);
+        this.begin = begin;
+        this.end = end;
+    }
+}
